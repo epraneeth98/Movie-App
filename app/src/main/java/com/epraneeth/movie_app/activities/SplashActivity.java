75d09b9607f2e3930a136e46dd8c107a0c6f0eb3
epraneeth98 <ts-praneeth.edubilli@rakuten.com>
2021-02-01 18:04:42 +0530

    Splash Screen:

--- app/src/main/java/com/epraneeth/movie_app/activities/SplashActivity.java
+++ app/src/main/java/com/epraneeth/movie_app/activities/SplashActivity.java
@@ -7,11 +7,13 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
 import android.widget.ProgressBar;
+import android.widget.Toolbar;
 
 import com.epraneeth.movie_app.R;
 
 public class SplashActivity extends AppCompatActivity {
     ProgressBar progressBar;
+    Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,6 +29,7 @@ public class SplashActivity extends AppCompatActivity {
             startActivity(i);
             progressBar = findViewById(R.id.progress_bar);
             progressBar.setVisibility(View.INVISIBLE);
+            //toolbar = findViewById(R.id.);
             finish();
         }, 1500);
     }
